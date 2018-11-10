9f435f3c0a2f7152a2366adb220069e463b44f04
Edward Tian <edwardtian@nat-oitwireless-inside-vapornet100-c-16621.Princeton.EDU>
2018-11-10 14:55:41 -0500

    .e

--- post-scrape/selenium-gil.py
+++ post-scrape/selenium-gil.py
@@ -1,6 +1,8 @@
 import selenium
 from selenium import webdriver
 import json
+import os
+import time
 
 chromeOptions = webdriver.ChromeOptions()
 chromeOptions.add_argument('--start-maximized')
@@ -10,7 +12,11 @@ chromeOptions.add_argument('--no-sandbox')
 chromeOptions.add_argument('--mute-audio')
 chromeOptions.add_argument('--log-level=3')
 chromeOptions.add_argument('--silent')
-driver = webdriver.Chrome('chromedriver.exe', chrome_options=chromeOptions)
+
+#lines 16 and 17 are only necessary for Mac OS
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+DRIVER_BIN =os.path.join(PROJECT_ROOT,"chromedriver")
+driver = webdriver.Chrome(DRIVER_BIN, chrome_options=chromeOptions)
 driver.implicitly_wait(0)
 
 driver.get('https://realtalk-princeton.tumblr.com/page/1')
@@ -21,6 +27,7 @@ while True:
     dates = driver.find_elements_by_class_name('post-date')
     questions = driver.find_elements_by_class_name('question-box')
     answers = driver.find_elements_by_class_name('post-content')
+    # finds elements returns arrays
 
     for a in range(0, len(questions)):
         date = dates[a].get_attribute('innerText')
@@ -30,14 +37,15 @@ while True:
         print(question)
         print(answer)
         posts.append([date, question, answer])
+    break
 
-    next = driver.find_elements_by_id('next')
+    next = driver.find_elements_by_id('next') #id of the next button is next
     if len(next) == 0:
         break
-    next[0].click()
-
-driver.close()
+    next[0].click() #array 0 because next is an array
 
 # dump posts as json
 with open('posts.json', 'w', encoding='utf8') as outfile:
     json.dump(posts, outfile, ensure_ascii=False)
+
+driver.close()
