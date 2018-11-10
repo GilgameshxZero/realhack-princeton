ef457edbda29b85ba9b8195f773a7e20a0907294
Yang Yan <yangawesome@gmail.com>
2018-11-10 12:12:11 -0500

    prelim scraping

---
+++ post-scrape/selenium-gil.py
@@ -0,0 +1,43 @@
+import selenium
+from selenium import webdriver
+import json
+
+chromeOptions = webdriver.ChromeOptions()
+chromeOptions.add_argument('--start-maximized')
+chromeOptions.add_argument('--disable-dev-shm-usage')
+chromeOptions.add_argument('--disable-gpu')
+chromeOptions.add_argument('--no-sandbox')
+chromeOptions.add_argument('--mute-audio')
+chromeOptions.add_argument('--log-level=3')
+chromeOptions.add_argument('--silent')
+driver = webdriver.Chrome('chromedriver.exe', chrome_options=chromeOptions)
+driver.implicitly_wait(0)
+
+driver.get('https://realtalk-princeton.tumblr.com/')
+
+posts = []
+
+while True:
+    dates = driver.find_elements_by_class_name('post-date')
+    questions = driver.find_elements_by_class_name('question-box')
+    answers = driver.find_elements_by_class_name('post-content')
+
+    for a in range(0, len(questions)):
+        date = dates[a].get_attribute('innerText')
+        question = questions[a].get_attribute('innerText')
+        answer = answers[a].get_attribute('innerText')
+        print(date)
+        print(question)
+        print(answer)
+        posts.append([date, question, answer])
+
+    next = driver.find_element_by_id('next')
+    if next == None:
+        break
+    next.click()
+
+driver.close()
+
+# dump posts as json
+with open('posts.json', 'w') as outfile:
+    json.dump(posts, outfile)
